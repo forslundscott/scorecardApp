7abfe6dc1bfe4361219cbfe64ba8183b10f894ae
Forslund <X002970@PWUS.us>
2023-11-02 16:59:12 -0400

    activeGame format mostly done, Game list is just started, touch feature added, SQL connections added

--- public/main.js
+++ public/main.js
@@ -1,3 +1,66 @@
+
+var eventLog = [
+
+]
+function sendGameData(){
+    
+}
+if(sessionStorage.gameInfo == 'undefined' || sessionStorage.gameInfo == '' || sessionStorage.gameInfo == null) {
+    var gameInfo = {
+        name: '',
+        homeTeam: '',
+        awayTeam: '',
+        period: 1,
+        time: ''
+    }
+}else {
+    gameInfo = JSON.parse(sessionStorage.gameInfo)
+}
+window.addEventListener('beforeunload', (event) => {
+    sessionStorage.setItem('gameInfo',JSON.stringify(gameInfo))
+  });
+class Game {
+    constructor(){
+        this.id = ''
+        this.homeTeam = ''
+        this.awayTeam = ''
+        this.period = 1
+        this.clockTime = ''
+        this.log = []
+    }
+}
+class Team {
+    constructor(){
+        this.id = ''
+        this.name = ''
+        this.keeper = ''
+        this.players = []
+        this.log = []
+    }
+}
+class Player {
+    constructor(){
+        this.id = ''
+        this.firstName = ''
+        this.lastName = ''
+        this.gender = ''
+        this.log = []
+    }
+}
+class GameEvent {
+    constructor(){
+        this.timeStamp = new Date(Date()).toISOString()
+        this.period = GameInfo.period
+        this.gameId = ''
+        this.teamId = ''
+        this.playerId = ''
+        this.type = ''
+        this.value = 0
+        this.clockTime = ''
+    }
+}
+
+
 document.addEventListener('DOMContentLoaded', function () {
     const eles = document.getElementsByClassName('fielderColumn')
     for(var i=0;i<eles.length;i++){
@@ -24,7 +87,9 @@ function setDragScroll(ele){
         };
 
         document.addEventListener('mousemove', mouseMoveHandler);
+        ele.addEventListener('ontouchmove', mouseMoveHandler);
         document.addEventListener('mouseup', mouseUpHandler);
+        ele.addEventListener('ontouchend', mouseUpHandler);
     };
 
     const mouseMoveHandler = function (e) {
@@ -42,11 +107,14 @@ function setDragScroll(ele){
         ele.style.removeProperty('user-select');
 
         document.removeEventListener('mousemove', mouseMoveHandler);
+        ele.removeEventListener('ontouchmove', mouseMoveHandler);
         document.removeEventListener('mouseup', mouseUpHandler);
+        ele.removeEventListener('ontouchend', mouseUpHandler);
     };
 
     // Attach the handler
     ele.addEventListener('mousedown', mouseDownHandler);
+    ele.addEventListener('ontouchstart', mouseDownHandler);
 }
 var distance = 15 * 60 * 1000;
 var x = setInterval(function() {
@@ -87,11 +155,66 @@ var x = setInterval(function() {
   }
 
 
-  function statHandler(statType, team){
+  function statHandler(statType, team, player, form){
+    console.log(form)
+    form.querySelector('[name="realTime"]').value = Date.now()
+    // console.log(form.querySelector('[name="realTime"]').value)
+    if(statType.includes('minus')){
+        form.querySelector('[name="value"]').value = '-1'
+    }else {
+        form.querySelector('[name="value"]').value = '1'
+    }
+    form.querySelector('[name="period"]').value = gameInfo.period
+    form.querySelector('[name="periodTime"]').value = document.getElementById('gameTimer').innerText
     switch(statType){
         case 'goal':
+            console.log(team)
             console.log(team + 'Score')
             document.getElementById(team + 'Score').innerText = Number(document.getElementById(team + 'Score').innerText) + 1
             break
     }
   }
+  // mouseup need to be monitored on a "global" element or we might miss it if
+// we move outside the original element.
+var isDown = false,
+isLong = false,
+target,                                         // which element was clicked
+longTID;
+function handleMouseDown() {
+    console.log('mouse down')
+//   this.innerHTML = "Mouse down...";
+  isDown = true;                                    // button status (any button here)
+  isLong = false;                                   // longpress status reset
+  target = this;                                    // store this as target element
+  clearTimeout(longTID);                            // clear any running timers
+  longTID = setTimeout(longPress, 1000); // create a new timer for this click
+};
+
+function handleMouseUp(e,callback, button) {
+  if (isDown && isLong) {                           // if a long press, cancel
+    isDown = false;                                 // clear in any case
+    e.preventDefault();                             // and ignore this event
+    return
+  }
+  
+  if (isDown) {                                     // if we came from down status:
+      clearTimeout(longTID);                        // clear timer to avoid false longpress
+      isDown = false;
+      console.log('Normal up')
+      callback.apply(this,[button.value,button.form.teamName.value,button.form.playerId.value,button.form])
+    //   target.innerHTML = "Normal up";               // for clicked element
+      target = null;
+  }
+};
+
+function longPress() {
+  isLong = true;
+  console.log('Long Press')
+//   this.innerHTML = "Long press";
+  // throw custom event or call code for long press
+}
+
+
+
+
+  
--- server.js
+++ server.js
@@ -6,7 +6,9 @@ let options = {}
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public',options))
 app.set('view-engine','ejs')
+var eventLog = [
 
+]
 // Helpers and Routes
 const functions = require('./helpers/functions')
 // const forms = require('./routes/forms')
@@ -14,55 +16,285 @@ const functions = require('./helpers/functions')
 app.locals.functions = functions
 
 // var connection = functions.getAccess()
+var gameInfo = {
+    name: '',
+    homeTeam: '',
+    awayTeam: '',
+    period: 1,
+    time: ''
+}
+class Game {
+    constructor(){
+        this.name = ''
+        this.homeTeam = new Team
+        this.awayTeam = new Team
+        this.period = 1
+        this.time = ''
+    }
+}
+class GameEvent {
+    constructor(){
+        this.timeStamp = ''
+        this.period = 1
+        this.team = ''
+        this.player = ''
+        this.event = ''
+        this.value = 0
+    }
+}
+class Team {
+    constructor(){
+        this.keeper = ''
+        this.goalsFor = 0
+        this.goalsAgainst = 0
+        this.assits = 0
+        this.saves = 0
+        this.players = {}
+    }
+}
+class Player {
+    constructor(){
+        this.firstName = ''
+        this.lastName = ''
+        this.goals = 0
+        this.assits = 0
+        this.ownGoals = 0
+    }
+}
 
 var team1 = {
-    name: 'HON',
-    keeper: 'Morgan',
+    id: "BEER",
+    name: "We're Here for the Beer",
+    keeper: {id: 'MorganAbney',
+                firstName: 'Morgan',
+                lastName: 'Abney'
+            },
     players: [
-        'Scott',
-        'Taylor',
-        'J',
-        'Nandan',
-        'Jeremy',
-        'Brendan',
-        'Moses',
-        'Mitch',
-        'Nikki',
-        'Yusif',
-        'Greg',
-        'Khalid'
+        {id: 'ScottForslund',
+            firstName: 'Scott',
+            lastName: 'Forslund'
+        },
+        {id: 'TaylorHori',
+            firstName: 'Taylor',
+            lastName: 'Hori'
+        },
+        {id: 'JustinTeel',
+            firstName: 'Justin',
+            lastName: 'Teel'
+        },
+        {id: 'NandanTandan',
+            firstName: 'Nandan',
+            lastName: 'Tandan'
+        },
+        {id: 'JeremyAshcroft',
+            firstName: 'Jeremy',
+            lastName: 'Ashcroft'
+        },
+        {id: 'BrendanBarnes',
+            firstName: 'Brendan',
+            lastName: 'Barnes'
+        },
+        {id: 'MitchBrown',
+            firstName: 'Mitch',
+            lastName: 'Brown'
+        },
+        {id: 'NikkiBrown',
+            firstName: 'Nikki',
+            lastName: 'Brown'
+        },
+        {id: 'GregKruger',
+            firstName: 'Greg',
+            lastName: 'Kruger'
+        },
+        {id: 'MorganAbney',
+            firstName: 'Morgan',
+            lastName: 'Abney'
+        }
     ]
 }
 
 var team2 = {
-    name: 'BSG',
-    keeper: 'Owen',
+    id: 'BSG',
+    name: 'The Branch SG',
+    keeper: {id: 'MorganAbney',
+                firstName: 'Morgan',
+                lastName: 'Abney'
+            },
     players: [
-        'Nicole',
-        'Ali',
-        'Mehdi',
-        'Stephani',
-        'Dan',
-        'Sammy',
-        'Patrick',
-        'Dan',
-        'Elias',
-        'Joaquin',
-        'Samuel'
+        {id: 'ScottForslund',
+            firstName: 'Scott',
+            lastName: 'Forslund'
+        },
+        {id: 'TaylorHori',
+            firstName: 'Taylor',
+            lastName: 'Hori'
+        },
+        {id: 'JustinTeel',
+            firstName: 'Justin',
+            lastName: 'Teel'
+        },
+        {id: 'NandanTandan',
+            firstName: 'Nandan',
+            lastName: 'Tandan'
+        },
+        {id: 'JeremyAshcroft',
+            firstName: 'Jeremy',
+            lastName: 'Ashcroft'
+        },
+        {id: 'BrendanBarnes',
+            firstName: 'Brendan',
+            lastName: 'Barnes'
+        },
+        {id: 'MitchBrown',
+            firstName: 'Mitch',
+            lastName: 'Brown'
+        },
+        {id: 'NikkiBrown',
+            firstName: 'Nikki',
+            lastName: 'Brown'
+        },
+        {id: 'GregKruger',
+            firstName: 'Greg',
+            lastName: 'Kruger'
+        },
+        {id: 'JosephAzako',
+            firstName: 'Joseph',
+            lastName: 'Azako'
+        },
+        {id: 'MorganAbney',
+            firstName: 'Morgan',
+            lastName: 'Abney'
+        }
     ]
 }
+var games = []
+function tempLoadGame(){
+    var activeGame = new Game
+    activeGame.homeTeam.keeper = 'MorganAbney'
+    activeGame.awayTeam.keeper = 'MorganAbney'
 
+    for(var i=0;i<team1.players.length;i++){
+        var newPlayer = new Player
+        newPlayer.firstName = team1.players[i].firstName
+        newPlayer.lastName = team1.players[i].lastName
+        newPlayer.id = newPlayer.firstName + newPlayer.lastName
+        activeGame.homeTeam.players[newPlayer.id]= newPlayer
+    }
+    for(var i=0;i<team2.players.length;i++){
+        var newPlayer = new Player
+        newPlayer.firstName = team2.players[i].firstName
+        newPlayer.lastName = team2.players[i].lastName
+        newPlayer.id = newPlayer.firstName + newPlayer.lastName
+        activeGame.awayTeam.players[newPlayer.id]= newPlayer
+    }
+    console.log(activeGame.awayTeam.players[1])
+    for(var i=0;i<activeGame.awayTeam.players.length;i++){
+        console.log(activeGame.awayTeam.players)
+    }
+    
+}
+tempLoadGame()
 app.get(['/'], async (req,res)=>{
-    // res.render('index.ejs')  
-    res.render('smartphone.ejs') 
+    // res.render('index.ejs')
+    res.redirect('/activeGame')
+    // res.render('smartphone.ejs') 
 })
-app.get(['/display'], async (req,res)=>{
-    // res.render('index.ejs')  
+app.get(['/games'], async (req,res)=>{
+    // res.render('index.ejs')
+    const sql = require('mssql');
+    const config = {
+        server: 'PUSLMA0E',
+        database: 'scorecard',
+        user: 'sForslund',
+        password: 'Planbsk8!!8ksbnalP',
+        trustServerCertificate: true,
+    };
+
+    sql.connect(config).then(pool => {
+        // Query
+        return pool.request()
+            .query(`SELECT * FROM [scorecard].[dbo].[games$]`)
+    }).then(result => {
+        games = result.recordset
+        // console.log(games)
+        // console.log(result.recordset[0].Location)
+    }).catch(err => {
+        console.log(err)
+    // ... error checks
+    });  
+    console.log(req.route.path[0])
     var data = {
         teams: [
             team1,
             team2
-        ]
+        ],
+        page: req.route.path[0].replace('/','')
+    }
+    res.render('index.ejs',data) 
+})
+app.get(['/activeGame'], async (req,res)=>{
+    // res.render('index.ejs')
+    const sql = require('mssql');
+    const config = {
+        server: 'PUSLMA0E',
+        database: 'scorecard',
+        user: 'sForslund',
+        password: 'Planbsk8!!8ksbnalP',
+        trustServerCertificate: true,
+    };
+
+    sql.connect(config).then(pool => {
+        // Query
+        return pool.request()
+            .query(`SELECT * FROM [scorecard].[dbo].[players$] Where Team='BEER'`)
+    }).then(result => {
+        games = result.recordset
+        // console.log(games)
+        // console.log(result.recordset[0].Location)
+    }).catch(err => {
+        console.log(err)
+    // ... error checks
+    });  
+    console.log(games)
+    var data = {
+        teams: [
+            team1,
+            team2
+        ],
+        page: req.route.path[0].replace('/','')
+    }
+    res.render('index.ejs',data) 
+})
+app.get(['/display'], async (req,res)=>{
+    // res.render('index.ejs')
+    const sql = require('mssql');
+    const config = {
+        server: 'PUSLMA0E',
+        database: 'scorecard',
+        user: 'sForslund',
+        password: 'Planbsk8!!8ksbnalP',
+        trustServerCertificate: true,
+    };
+
+    sql.connect(config).then(pool => {
+        // Query
+        return pool.request()
+            .query(`SELECT * FROM [scorecard].[dbo].[players$] Where Team='BEER'`)
+    }).then(result => {
+        games = result.recordset
+        // console.log(games)
+        // console.log(result.recordset[0].Location)
+    }).catch(err => {
+        console.log(err)
+    // ... error checks
+    });  
+    console.log(games)
+    var data = {
+        teams: [
+            team1,
+            team2
+        ],
+        page: req.query.page
     }
     res.render('index.ejs',data) 
 })
@@ -70,6 +302,38 @@ app.post(['/'], async (req,res)=>{
     // res.render('index.ejs')
     res.render('smartphone.ejs') 
 })
+app.post(['/eventLog'], async (req,res)=>{
+    eventLog.push(req.body)
+    console.log(eventLog)
+    res.sendStatus(204)
+})
+
+
+app.get(['/schedule'], async (req,res)=>{
+    const sql = require('mssql');
+    const config = {
+        server: 'PUSLMA0E',
+        database: 'scorecard',
+        user: 'sForslund',
+        password: 'Planbsk8!!8ksbnalP',
+        trustServerCertificate: true,
+    };
+
+sql.connect(config).then(pool => {
+    // Query
+    return pool.request()
+        .query(`SELECT top 1 * FROM [scorecard].[dbo].[games1$]`)
+}).then(result => {
+    console.log(result.recordset[0].Location)
+}).catch(err => {
+    console.log(err)
+  // ... error checks
+});
+console.log('test')
+    // res.sendStatus(204)
+})
 app.listen(port, function(err){
     // if (err) console.log(err);
  })
+// eventLog
+//  res.sendStatus(200)
