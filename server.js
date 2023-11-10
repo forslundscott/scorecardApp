48a43e7fdce760fd7dd8f4a7eb048afce962b1c7
Scott Forslund <94809089+forslundscott@users.noreply.github.com>
2023-11-10 16:19:15 -0500

    added bookings, keeper switch, and switch team for player event form; added subs and signed waiver to add player form

--- server.js
+++ server.js
@@ -77,15 +77,6 @@ app.get(['/games'], async (req,res)=>{
     res.render('index.ejs',data) 
 })
 app.get(['/activeGame'], async (req,res)=>{
-    // res.render('index.ejs')
-    // const sql = require('mssql');
-    // const config = {
-    //     server: 'scott-HP-Z420-Workstation',
-    //     database: 'scorecard',
-    //     user: 'SRF',
-    //     password: 'Planbsk8!!8ksbnalP',
-    //     trustServerCertificate: true,
-    // };
 
     await sql.connect(config).then(pool => {
         // Query
@@ -112,7 +103,7 @@ app.get(['/activeGame'], async (req,res)=>{
     await sql.connect(config).then(pool => {
         // Query
         return pool.request()
-            .query(`SELECT SUM(value) as score FROM [scorecard].[dbo].[eventLog] WHERE event_Id = ${req.query.Event_ID} and (teamName = '${req.query.Team2_ID}' and type = 'owngoal') or (teamName = '${req.query.Team1_ID}' and type = 'goal') GROUP BY event_id`)
+            .query(`SELECT SUM(value) as score FROM [scorecard].[dbo].[eventLog] WHERE event_Id = ${req.query.Event_ID} and ((teamName = '${req.query.Team2_ID}' and type = 'owngoal') or (teamName = '${req.query.Team1_ID}' and type = 'goal')) GROUP BY event_id`)
     }).then(result => {
         if(result.recordset.length == 0){
             team1.score = 0
@@ -128,7 +119,7 @@ app.get(['/activeGame'], async (req,res)=>{
     await sql.connect(config).then(pool => {
         // Query
         return pool.request()
-            .query(`SELECT SUM(value) as score FROM [scorecard].[dbo].[eventLog] WHERE event_Id = ${req.query.Event_ID} and (teamName = '${req.query.Team1_ID}' and type = 'owngoal') or (teamName = '${req.query.Team2_ID}' and type = 'goal') GROUP BY event_id`)
+            .query(`SELECT SUM(value) as score FROM [scorecard].[dbo].[eventLog] WHERE event_Id = ${req.query.Event_ID} and ((teamName = '${req.query.Team1_ID}' and type = 'owngoal') or (teamName = '${req.query.Team2_ID}' and type = 'goal')) GROUP BY event_id`)
     }).then(result => {
         if(result.recordset.length == 0){
             team2.score = 0
@@ -203,6 +194,35 @@ app.post(['/addPlayer'], async (req,res)=>{
     res.redirect('back')
 })
 
+// var x = setInterval(function() {
+//     ;
+//     // Get today's date and time
+//     // var now = new Date().getTime();
+      
+//     // Find the distance between now and the count down date
+//     if(document.getElementById('timerButton').innerText == 'Stop'){
+//         distance = distance - 1000;
+        
+//         // Time calculations for days, hours, minutes and seconds
+//         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+//         var seconds = Math.floor((distance % (1000 * 60)) / 1000).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+        
+//         // Output the result in an element with id="demo"
+//         document.getElementById("gameTimer").innerHTML = minutes + ":" + seconds;
+        
+//         // If the count down is over, write some text 
+//         if (distance < 0) {
+//             clearInterval(x);
+//             gameInfo.period = gameInfo.period + 1
+//             // gameInfo.time = '15:00'
+            
+//         document.getElementById("gameTimer").innerHTML = "15:00";
+//         document.getElementById('timerButton').innerHTML = 'Start'
+//         location.reload()
+//         // document.getElementById('timerButton').innerText = 'Start'
+//         }
+//     }
+//   }, 1000);
 
 app.listen(port, function(err){
     // if (err) console.log(err);
